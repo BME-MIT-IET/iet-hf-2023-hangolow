3a6945204067b4f96ad781044779e1b663cc1e0a
kisb9999 <kisb20000909@gmail.com>
2023-05-29 17:24:34 +0200

    Add move test

---
+++ tests/java/com/examples/cucumber/MoveStepDefs.java
@@ -0,0 +1,106 @@
+package com.examples.cucumber;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.sl.In;
+import model.Game;
+import model.Virologist;
+import model.agents.Agent;
+import model.agents.Bear;
+import model.agents.Stun;
+import model.codes.BlockCode;
+import model.codes.StunCode;
+import model.map.Field;
+import model.map.InfectedLaboratory;
+import model.map.Warehouse;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoveStepDefs {
+    Field prevField;
+
+    @Given("InitMove")
+    public void initmove(){
+        TestWorldContext context = TestWorldContext.Instance();
+        context.userPlayer = new Virologist();
+        context.currentField = new Field();
+        context.userPlayer.SetField(context.currentField);
+    }
+
+    @When("the player tries to move")
+    public void thePlayerTriesToMove() {
+        TestWorldContext context = TestWorldContext.Instance();
+        Field f = new Field();
+        prevField = context.userPlayer.getField();
+        context.userPlayer.Move(f);
+    }
+
+    @Then("the player is moved to the chosen field")
+    public void thePlayerIsMovedToTheChosenField() {
+        TestWorldContext context = TestWorldContext.Instance();
+        assertNotEquals(prevField, context.userPlayer.getField());
+    }
+
+    @And("the player has {int} remaining moves")
+    public void thePlayerHasRemainingMoves(int arg0) {
+        TestWorldContext context = TestWorldContext.Instance();
+        context.userPlayer.SetActionCount(arg0);
+    }
+
+    @Then("the player stays on the same field as before")
+    public void thePlayerStaysOnTheSameFieldAsBefore() {
+        TestWorldContext context = TestWorldContext.Instance();
+        assertEquals(prevField, context.userPlayer.getField());
+    }
+
+    @And("the player is infected with the bear virus")
+    public void thePlayerIsInfectedWithTheBearVirus() {
+        TestWorldContext context = TestWorldContext.Instance();
+        boolean hasBear = false;
+        for(var virus : context.userPlayer.GetAgents()){
+            if(virus.getClass() == Bear.class)
+                hasBear = true;
+        }
+        assertTrue(hasBear);
+    }
+
+    @Then("the player is moved to a random field")
+    public void thePlayerIsMovedToARandomField() {
+        TestWorldContext context = TestWorldContext.Instance();
+        assertNotEquals(prevField, context.userPlayer.getField());
+    }
+
+    @And("if there are other players on the field, they become infected as well")
+    public void ifThereAreOtherPlayersOnTheFieldTheyBecomeInfectedAsWell() {
+        TestWorldContext context = TestWorldContext.Instance();
+        Virologist v = new Virologist();
+        Field f = context.userPlayer.getField();
+        f.AddVirologist(v);
+        v.AddAgent(new Bear());
+    }
+
+    @And("the field has a player infected with the bear virus")
+    public void theFieldHasAPlayerInfectedWithTheBearVirus() {
+        TestWorldContext context = TestWorldContext.Instance();
+        Field startField = new Field();
+        context.userPlayer.SetField(startField);
+        for(int i = 0; i < 3; i++){
+            Field neighbouringField = new Field();
+            Virologist v = new Virologist();
+            v.AddAgent(new Bear());
+            neighbouringField.AddVirologist(v);
+            startField.AddNeighbour(neighbouringField);
+        }
+        context.userPlayer.Move(startField.GetNeighbours().get(0));
+    }
+
+    @When("the player tries to move to an infected laboratory field")
+    public void thePlayerTriesToMoveToAnInfectedLaboratoryField() {
+        TestWorldContext context = TestWorldContext.Instance();
+        InfectedLaboratory infected = new InfectedLaboratory(new StunCode());
+        context.userPlayer.Move(infected);
+    }
+}
--- tests/resources/features/moveFeature.feature
+++ tests/resources/features/moveFeature.feature
@@ -3,52 +3,42 @@ Feature: Move
   I want to move to a different field
 
   Scenario: A successful move to another field with moves remaining
-    Given the player has more than 0 remaining moves
-    When the user clicks on "Actions"
-    And the user clicks on "move"
-    And chooses a field
+    Given InitMove
+    When the player tries to move
     Then the player is moved to the chosen field
     And the player loses an action
 
-  Scenario: A unsuccessful move to another field with no moves remaining
-    Given the player has 0 remaining moves
-    When the user clicks on "Actions"
-    And the user clicks on "move"
-    And chooses a field
+  Scenario: An unsuccessful move to another field with no moves remaining
+    Given InitMove
+    And the player has 0 remaining moves
+    When the player tries to move
     Then the player stays on the same field as before
     And the player loses an action
 
   Scenario: The player tries to move, but is stunned
-    Given the player is stunned
-    When the user clicks on "Actions"
-    And the user clicks on "move"
-    And chooses a field
+    Given InitMove
+    And the player is stunned
+    When the player tries to move
     Then the player stays on the same field as before
     And the player loses an action
 
   Scenario: The player is infected with the bear virus
-    Given the player is infected with the bear virus
-    When the user clicks on "Actions"
-    And the user clicks on "move"
-    And chooses a field
+    Given InitMove
+    When the player tries to move
     Then the player is moved to a random field
     And if there are other players on the field, they become infected as well
     And the player loses an action
 
   Scenario: The player moves to an infected laboratory
-    Given the player has more than 0 remaining moves
-    When the user clicks on "Actions"
-    And the user clicks on "move"
-    And chooses an infected laboratory field
+    Given InitMove
+    When the player tries to move to an infected laboratory field
     Then the player is moved to the chosen field
     And the player is infected with the bear virus
     And the player loses an action
 
   Scenario: The player moves to an infected laboratory
-    Given the player has more than 0 remaining moves
-    When the user clicks on "Actions"
-    And the user clicks on "move"
-    And chooses a field
+    Given InitMove
+    When the player tries to move
     And the field has a player infected with the bear virus
     Then the player is moved to the chosen field
     And the player is infected with the bear virus
